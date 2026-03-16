28e2986d5a5d5148664e309e94fc21392719412a
Your Name <your.email@example.com>
2026-03-17 02:36:13 +0300

    добавляю отслеживание неактивных товаров

--- src/main/java/com/back/minecraftback/controller/AuthController.java
+++ src/main/java/com/back/minecraftback/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("auth")
+@RequestMapping(path = { "auth", "/api/auth" })
 public class AuthController {
     private final AdminUsersService adminUsersService;
 
